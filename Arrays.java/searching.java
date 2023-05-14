4bed7e4824e170dc4f78159747cf322038bc9950
Rishita <125037556+rishitaa04@users.noreply.github.com>
2023-05-14 21:39:31 +0530

    Add files via upload

---
+++ Arrays.java/searching.java
@@ -0,0 +1,16 @@
+// package Arrays.java;
+
+public class searching {
+    public static void main(String arhs[]) {
+        int array[] = { 2, 7, 19, 4, 0 };
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                System.out.print(i);
+            }
+            // else {
+            // System.out.print("Not Found");
+            // }
+
+        }
+    }
+}
---
+++ Arrays.java/sorting.java
@@ -0,0 +1,5 @@
+// package Arrays.java;
+
+public class sorting {
+
+}
