1f7966955a66e005b1d3d22b0bdf052dc2272b10
Rishita <125037556+rishitaa04@users.noreply.github.com>
2023-05-20 09:48:44 +0530

    Add files via upload

---
+++ Stringbuilder.java/reverseastring.java
@@ -0,0 +1,16 @@
+// package ST3.java.Stringbuilder.java;
+
+import java.util.Scanner;
+
+public class reverseastring {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            // char c = (char) ('a' + i);
+            // builder.append(c);
+        }
+        System.out.println(builder.toString());
+        sc.close();
+    }
+}
